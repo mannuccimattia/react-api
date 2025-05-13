b1e3a156f6c7812840a437cf799a1c4384c15ebe
mannuccimattia <mannuccimattia@gmail.com>
2025-05-13 14:55:03 +0200

    [UPD] Done actors list render - raw, unstyled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const App = () => {
-  return (
-    <div>
 
-    </div>
+  const [actors, setActors] = useState([]);
+
+  const fetchActors = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actors/").then(res => setActors(res.data)).catch(error => console.error("Errore di caricamento Attori ", error));
+  }
+
+  useEffect(() => {
+    fetchActors();
+  }, [])
+
+  return (
+    <>
+      <div className="container">
+        <header className='text-center my-4'>
+          <h1>Cast Fetching</h1>
+        </header>
+
+        <main>
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-6 text-center">
+              <h3>Attori</h3>
+              <div className="row g-3">
+                {actors.map(actor => (
+                  <div
+                    className="col-12"
+                    key={`actor-${actor.id}`}
+                  >
+                    <div className="card d-flex">
+                      <div className="act-image">
+                        <img
+                          src={actor.image}
+                          alt={`actor ${actor.name}`}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="col-12 col-md-6 text-center">
+              <h3>Attrici</h3>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
 
